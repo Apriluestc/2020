47dbc980fcaa6b8e44e45e0e65e166e9996010ff
Shiny-Man <239194147@qq.com>
2019-08-06 23:03:43 +0800

    2020 届秋招笔试面试经验合集

--- upload.sh
+++ upload.sh
@@ -7,11 +7,8 @@ git add .github/
 git add 2020秋招面经/
 git add 2020秋招笔经/
 git add 剑指offer/
-git add 字节跳动后端研发笔试/
 git add doc/
-git add 2020届秋招笔试编程题集/
 git add 项目/
 git add pics/
-git add 腾讯后端研发笔试/
 git commit -m "2020 届秋招笔试面试经验合集"
 git push
