fe882173107d0fdfad5d229ce4d30ae3a80fb4cb
Shiny-Man <239194147@qq.com>
2019-08-10 10:32:10 +0800

    2020 届秋招笔试面试经验合集

--- 2020届秋招笔试编程题集/coding/dp02.cpp
+++ 2020届秋招笔试编程题集/coding/dp02.cpp
@@ -27,12 +27,23 @@ using namespace std;
 
 int main()
 {
+    // N 物品种类
+    // C 背包容量
     int N, C;
     while (cin >> N >> C) {
+        // W 物品体积或者花费或者重量
         vector<int> W(N + 1, 0);
         for (int i = 1; i <= N; i++) {
             cin >> W[i];
         }
+        vector<int> dp(C + 1, 0);
+        for (int i = 1; i <= N; i++) {
+            //  注意逆序保证每件物品只取一次
+            for (int j = C; j >= W[i]; j--) {
+                dp[j] = max(dp[j], dp[j-W[i]] + W[i]);
+            }
+        }
+        cout << dp[C] << endl;
     }
     return 0;
 }
